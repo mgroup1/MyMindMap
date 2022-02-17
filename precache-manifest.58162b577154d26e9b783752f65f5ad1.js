8a945676dec126c8404c7d44e89cb28a35c78380
mgroup1 <99913864+mgroup1@users.noreply.github.com>
2022-02-17 22:49:11 +0200

    Add files via upload

---
+++ precache-manifest.58162b577154d26e9b783752f65f5ad1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8109ccac48a2dad8f7",
+    "url": "/MyMindMap/css/app.3fecdae5.css"
+  },
+  {
+    "revision": "880646ee2f23a1019d5c",
+    "url": "/MyMindMap/css/chunk-7ba51edb.9b62179c.css"
+  },
+  {
+    "revision": "e104fea27a435d40ab85",
+    "url": "/MyMindMap/css/chunk-977022ee.87c3901d.css"
+  },
+  {
+    "revision": "f3261f0b618108cc6ed1",
+    "url": "/MyMindMap/css/chunk-vendors.d329fe96.css"
+  },
+  {
+    "revision": "51148f72a200d906cb1debde4d76367c",
+    "url": "/MyMindMap/img/logo.51148f72.svg"
+  },
+  {
+    "revision": "6b0a9ecb10e8b047d093ae94e5e232a0",
+    "url": "/MyMindMap/img/logo2.6b0a9ecb.svg"
+  },
+  {
+    "revision": "2f32cccc6c377207775c96ccde54aca8",
+    "url": "/MyMindMap/index.html"
+  },
+  {
+    "revision": "af8109ccac48a2dad8f7",
+    "url": "/MyMindMap/js/app.e34bca73.js"
+  },
+  {
+    "revision": "880646ee2f23a1019d5c",
+    "url": "/MyMindMap/js/chunk-7ba51edb.c8671053.js"
+  },
+  {
+    "revision": "e104fea27a435d40ab85",
+    "url": "/MyMindMap/js/chunk-977022ee.a6b6a454.js"
+  },
+  {
+    "revision": "f3261f0b618108cc6ed1",
+    "url": "/MyMindMap/js/chunk-vendors.1e40bf9a.js"
+  },
+  {
+    "revision": "72ed0d4820980a6a965609b57d75071e",
+    "url": "/MyMindMap/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MyMindMap/robots.txt"
+  }
+]);
